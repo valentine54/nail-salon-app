ea306ac35c7e9afecd10294f60a3db6379047475
valentine54 <wanjiku.valentine@strathmore.edu>
2026-05-01 20:54:07 +0300

    removed bacjend

--- nail-salon/backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
-router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'appointments', views.AppointmentViewSet)
-urlpatterns = [path('login/', views.login_view), path('', include(router.urls))]
